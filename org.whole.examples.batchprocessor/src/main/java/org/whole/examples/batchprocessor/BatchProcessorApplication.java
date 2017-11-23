e4161bf756ac7c2c48e0176d2c3480d7573db150
Enrico Persiani <enrico.persiani@metatis.com>
2017-11-23 17:57:55 +0100

    add batch processor example

---
+++ org.whole.examples.batchprocessor/src/main/java/org/whole/examples/batchprocessor/BatchProcessorApplication.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright 2004-2016 Riccardo Solmi. All rights reserved.
+ * This file is part of the Whole Platform.
+ *
+ * The Whole Platform is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Whole Platform is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Whole Platform. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.whole.examples.batchprocessor;
+
+import static org.apache.commons.io.filefilter.FileFilterUtils.and;
+import static org.apache.commons.io.filefilter.FileFilterUtils.or;
+import static org.apache.commons.io.filefilter.FileFilterUtils.suffixFileFilter;
+import static org.apache.commons.io.filefilter.FileFilterUtils.trueFileFilter;
+
+import java.io.File;
+import java.util.Iterator;
+
+import org.apache.commons.io.FileUtils;
+import org.whole.lang.bindings.BindingManagerFactory;
+import org.whole.lang.bindings.IBindingManager;
+import org.whole.lang.codebase.FilePersistenceProvider;
+import org.whole.lang.codebase.IPersistenceKit;
+import org.whole.lang.codebase.IPersistenceProvider;
+import org.whole.lang.java.codebase.JavaBuilderPersistenceKit;
+import org.whole.lang.lifecycle.IHistoryManager;
+import org.whole.lang.model.IEntity;
+import org.whole.lang.reflect.ReflectionFactory;
+import org.whole.lang.util.BehaviorUtils;
+import org.whole.lang.xml.codebase.XmlBuilderPersistenceKit;
+
+/**
+ * @author Enrico Persiani
+ */
+public class BatchProcessorApplication {
+	public static void main(String[] args) throws Exception {
+		if (args.length < 2 || args.length > 3) {
+			System.err.println("USAGE: whole-batch [-r|--result] <models-dir> <behavior-file> ");
+			System.exit(1);
+		}
+
+		boolean useResult = false;
+		int argsIndex = 0;
+		if ("-r".equals(args[argsIndex]) || "--result".equals(args[argsIndex])) {
+			argsIndex += 1;
+			useResult = true;
+		}
+		
+		File modelsDir = new File(args[argsIndex++]);
+		if (!modelsDir.exists() || !modelsDir.isDirectory()) {
+			System.err.println("<models-dir> either doesn't exist or is not a directory");
+			System.exit(3);
+		}
+
+		ReflectionFactory.deployWholePlatform();
+		IEntity behavior = null;
+		try {
+			File behaviorFile = new File(args[argsIndex++]);
+			behavior = getPersistenceKit(behaviorFile.getName()).readModel(new FilePersistenceProvider(behaviorFile));
+		} catch (Exception e) {
+			System.err.println("<behavior-file> either doesn't exist or is not a valid whole model");
+			System.exit(2);
+		}
+
+		Iterator<File> iterateFiles = FileUtils.iterateFiles(modelsDir, 
+				or(suffixFileFilter(".xwl"), and(suffixFileFilter(".java"), JavaBuilderFilter.INSTANCE)),
+				trueFileFilter());
+		while (iterateFiles.hasNext()) {
+			File file = iterateFiles.next();
+			String fileName = file.getName();
+			if ("NotationsLibrary.xwl".equals(fileName))
+				fileName.length();
+			if ("CommonsModel.xwl".equals(fileName))
+				fileName.length();
+
+			System.out.print("processing '"+fileName+"'... ");
+			try {
+				IPersistenceKit persistenceKit = getPersistenceKit(fileName);
+				IEntity entity = persistenceKit.readModel(new FilePersistenceProvider(file));
+
+				IHistoryManager historyManager = ReflectionFactory.getHistoryManager(entity);
+				historyManager.setHistoryEnabled(true);
+
+				IBindingManager arguments = BindingManagerFactory.instance.createArguments();
+				arguments.wDef("self", entity);
+				IEntity result = BehaviorUtils.evaluate(behavior, 0, arguments);
+
+				if (historyManager.getUndoSize() > 0 || (useResult && result != null)) {
+					IPersistenceProvider pp = new FilePersistenceProvider(file);
+					pp.getBindings().wDefValue("packageName", JavaBuilderFilter.INSTANCE.getPackageName(file));
+					persistenceKit.writeModel(useResult ? result : entity, pp);
+					System.out.println("modified.");
+				} else
+					System.out.println("unchanged.");
+			} catch (Exception e) {
+				System.out.println("failed.");
+			}
+		}
+	}
+
+	public static IPersistenceKit getPersistenceKit(String fileName) {
+		return fileName.endsWith(".xwl") ? 
+				XmlBuilderPersistenceKit.instance() : JavaBuilderPersistenceKit.instance();
+	}
+}
---
+++ org.whole.examples.batchprocessor/src/main/java/org/whole/examples/batchprocessor/JavaBuilderFilter.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2004-2016 Riccardo Solmi. All rights reserved.
+ * This file is part of the Whole Platform.
+ *
+ * The Whole Platform is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Whole Platform is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Whole Platform. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.whole.examples.batchprocessor;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.LineIterator;
+import org.apache.commons.io.filefilter.AbstractFileFilter;
+
+/**
+ * @author Enrico Persiani
+ */
+public class JavaBuilderFilter extends AbstractFileFilter {
+	private static final String IMPORT_REGEX = "package\\s+([^;]+);";
+
+	public static final JavaBuilderFilter INSTANCE = new JavaBuilderFilter(10);
+
+	protected int maxLines;
+	protected JavaBuilderFilter(int maxLines) {
+		this.maxLines = maxLines;
+	}
+
+	@Override
+	public boolean accept(File file) {
+		return getInitialLines(file).stream()
+				.anyMatch((line) -> line.contains(" extends AbstractTemplateFactory<"));
+	}
+
+	public String getPackageName(File file) {
+		Pattern importPattern = Pattern.compile(IMPORT_REGEX);
+		return getInitialLines(file).stream()
+				.flatMap((line) -> {
+					Matcher matcher = importPattern.matcher(line);
+					return matcher.matches() ? Stream.of(matcher.group(1)) : null;
+				}).findAny().orElse(null);
+	}
+
+	protected List<String> getInitialLines(File file) {
+		List<String> lines = new ArrayList<String>(maxLines);
+		try {
+			int count = 0;
+			LineIterator iterator = IOUtils.lineIterator(FileUtils.openInputStream(file), Charset.defaultCharset());
+			while (iterator.hasNext() && count++ < maxLines) {
+				lines.add(iterator.next());
+			}
+		} catch (Exception e) {
+			// safely ignore
+		}
+		return lines;
+	}
+}
